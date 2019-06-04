68318f1a8af9fb9e5b19e9dbb924b6f54d197a18
VA <varavindhan2010@gmail.com>
2019-06-04 11:19:43 +0530

    Designed Event/Workshop Card

--- app/src/main/java/com/example/ieee_sb/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/ieee_sb/Fragments/HomeFragment.java
@@ -7,19 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.example.ieee_sb.R;
 
 public class HomeFragment extends Fragment {
 
-    private Button logout;
-    private ImageView backdrop;
-    private LinearLayout splash,home;
-    private Animation frombottom;
     private View view;
 
     @Nullable
