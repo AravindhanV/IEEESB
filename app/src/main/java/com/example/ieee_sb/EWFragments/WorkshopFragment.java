378f3595ff4973428e0252c7a5138a9ba933eaee
VA <varavindhan2010@gmail.com>
2019-06-08 20:10:13 +0530

    Workshops also pull from Firebase

--- app/src/main/java/com/example/ieee_sb/EWFragments/WorkshopFragment.java
+++ app/src/main/java/com/example/ieee_sb/EWFragments/WorkshopFragment.java
@@ -12,26 +12,85 @@ import android.view.ViewGroup;
 
 import com.example.ieee_sb.Event;
 import com.example.ieee_sb.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class WorkshopFragment extends Fragment {
 
-    private ArrayList<Event> events;
+    private ArrayList<Event> workshops;
     private View view;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView.Adapter adapter;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth firebaseAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.activity_workshops, container, false);
         recyclerView = view.findViewById(R.id.workshop_recycler);
-        events = new ArrayList<>();
-        events.add(new Event("Techquilla",new ArrayList<String>(),"Description Here",1,"FEB",2019,"3:30 PM"));
-        events.add(new Event("Wit Wars 2.0",new ArrayList<String>(),"Description Here",5,"MAY",2020,"4:15 PM"));
+        workshops = new ArrayList<>();
+//        events.add(new Event("Techquilla",new ArrayList<String>(),"Description Here",1,"FEB",2019,"3:30 PM"));
+//        events.add(new Event("Wit Wars 2.0",new ArrayList<String>(),"Description Here",5,"MAY",2020,"4:15 PM"));
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference = firebaseDatabase.getReference("/workshops");
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                workshops.add(dataSnapshot.getValue(Event.class));
+                refreshList();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s){
+                Event e = dataSnapshot.getValue(Event.class);
+                workshops.set(searchEvent(e.getID()),e);
+                refreshList();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                Event e = dataSnapshot.getValue(Event.class);
+                workshops.remove(searchEvent(e.getID()));
+                refreshList();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+            public int searchEvent(String s){
+                int i=0;
+                for (Event e : workshops) {
+                    if (e.getID().equals(s)) {
+                        break;
+                    }
+                    i++;
+                }
+                return i;
+            }
+
+            public void refreshList(){
+                adapter = new EWAdapter(workshops);
+                recyclerView.setAdapter(adapter);
+            }
+        });
 
         layoutManager = new LinearLayoutManager(getActivity());
-        adapter = new EWAdapter(events);
+        adapter = new EWAdapter(workshops);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
         return view;
