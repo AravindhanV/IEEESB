03a326e52f847481a90fa737d0c595ac13f7ed8e
VA <varavindhan2010@gmail.com>
2019-06-17 11:50:08 +0530

    Refactored Event page a little and added scroll feature to detail page

--- app/src/main/java/com/example/ieee_sb/DetailPopUpActivity.java
+++ app/src/main/java/com/example/ieee_sb/DetailPopUpActivity.java
@@ -7,11 +7,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class DetailPopUpActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ViewPager viewPager;
+    private ImageButton left,right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +22,48 @@ public class DetailPopUpActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.my_toolbar);
         viewPager = findViewById(R.id.detail_contact_list);
+        left = findViewById(R.id.detail_left_arrow);
+        right = findViewById(R.id.detail_right_arrow);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        OrganizerAdapter adapter = new OrganizerAdapter(getSupportFragmentManager());
+        final OrganizerAdapter adapter = new OrganizerAdapter(getSupportFragmentManager());
         adapter.add(new OrganizerFragment());
         adapter.add(new OrganizerFragment());
         adapter.add(new OrganizerFragment());
         adapter.add(new OrganizerFragment());
         adapter.add(new OrganizerFragment());
 
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if(i==0){
+                    left.setVisibility(View.INVISIBLE);
+                    right.setVisibility(View.VISIBLE);
+                }
+                else if(i==adapter.getCount()-1){
+                    left.setVisibility(View.VISIBLE);
+                    right.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    left.setVisibility(View.VISIBLE);
+                    right.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        left.setVisibility(View.INVISIBLE);
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(0);
 
